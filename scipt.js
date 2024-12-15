8c049c920f46e9f1c04fd19eeebf2055ff7d9202
Aomsin-A <yamadagijo@gmail.com>
2024-12-15 22:21:40 +0700

    4

---
+++ scipt.js
@@ -0,0 +1,5 @@
+document.getElementById('surpriseButton').addEventListener('click', function () {
+    const surprise = document.getElementById('surprise');
+    surprise.classList.toggle('hidden');
+  });
+  
